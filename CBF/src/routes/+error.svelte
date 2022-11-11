a14b1453d624624d2c4841beb899837872608398
amarhasic <76740130+amarhasic@users.noreply.github.com>
2022-11-11 16:06:00 +0100

    Initial updates

---
+++ CBF/src/routes/+error.svelte
@@ -0,0 +1,42 @@
+
+
+<div class="parent">
+    <div class="div1">
+        
+    </div>
+
+    <div class="div2"> 
+        <h1>An error has occured.</h1>
+	<h1> <span class="ascii">(╯°□°）╯︵ ┻━┻</span></h1>
+	<a href="/  ">Go back</a>
+    </div>
+    </div>
+
+
+    <style>
+
+
+
+.parent {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(5, 1fr);
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+    }
+    
+    .div1 { grid-area: 1 / 1 / 2 / 2;
+        padding-top: 15vw;
+    padding-bottom: 15vw;
+  background-image: url("/broken-button.png");  
+  background-repeat: no-repeat;
+  position: center;
+  background-attachment:fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size:contain;
+     }
+    .div2 { grid-area: 1 / 2 / 2 / 3; }
+
+</style>
